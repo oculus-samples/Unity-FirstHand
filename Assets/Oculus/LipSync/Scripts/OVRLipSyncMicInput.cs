f348513debc8e79ca44c442688962486599c637b
Rick Terrell <rterrell@meta.com>
2023-02-22 13:13:07 -0800

    Update and add license headers using LICENSELINT
    Summary: Using LICENSELINT in a safe way update all these files.
    
    Differential Revision: D43398300
    
    fbshipit-source-id: 58b287fb8db59e01d1822b4734bd1405b9a93830

--- Assets/Oculus/LipSync/Scripts/OVRLipSyncMicInput.cs
+++ Assets/Oculus/LipSync/Scripts/OVRLipSyncMicInput.cs
@@ -1,25 +1,22 @@
-/************************************************************************************
-Filename    :   OVRLipSyncMicInput.cs
-Content     :   Interface to microphone input
-Created     :   May 12, 2015
-Copyright   :   Copyright Facebook Technologies, LLC and its affiliates.
-                All rights reserved.
-
-Licensed under the Oculus Audio SDK License Version 3.3 (the "License");
-you may not use the Oculus Audio SDK except in compliance with the License,
-which is provided at the time of installation or download, or which
-otherwise accompanies this software in either electronic or hard copy form.
-
-You may obtain a copy of the License at
-
-https://developer.oculus.com/licenses/audio-3.3/
-
-Unless required by applicable law or agreed to in writing, the Oculus Audio SDK
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-************************************************************************************/
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * Licensed under the Oculus SDK License Agreement (the "License");
+ * you may not use the Oculus SDK except in compliance with the License,
+ * which is provided at the time of installation or download, or which
+ * otherwise accompanies this software in either electronic or hard copy form.
+ *
+ * You may obtain a copy of the License at
+ *
+ * https://developer.oculus.com/licenses/oculussdk/
+ *
+ * Unless required by applicable law or agreed to in writing, the Oculus SDK
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 using System;
 using UnityEngine;
--- Assets/Project/Scripts/Timeline/Tracks/ImageFill/ImageFillTrack.cs
+++ Assets/Project/Scripts/Timeline/Tracks/ImageFill/ImageFillTrack.cs
@@ -2,8 +2,20 @@
  * Copyright (c) Meta Platforms, Inc. and affiliates.
  * All rights reserved.
  *
- * Use of the material below is subject to the terms of the MIT License
- * https://github.com/oculus-samples/Unity-FirstHand/tree/main/Assets/Project/LICENSE.txt
+ * Licensed under the Oculus SDK License Agreement (the "License");
+ * you may not use the Oculus SDK except in compliance with the License,
+ * which is provided at the time of installation or download, or which
+ * otherwise accompanies this software in either electronic or hard copy form.
+ *
+ * You may obtain a copy of the License at
+ *
+ * https://developer.oculus.com/licenses/oculussdk/
+ *
+ * Unless required by applicable law or agreed to in writing, the Oculus SDK
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 using UnityEngine;
